5e61f1cd441f6da9894ccf852fc41aaa1d5acc29
Philippe DENIEL <philippe.deniel@cea.fr>
2016-07-07 01:03:03 +0200

    Start putting flesh on busybox's skeleton
    Signed-off-by: Philippe DENIEL <philippe.deniel@cea.fr>

---
+++ kvsns_shell/MakeCmd.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+
+ln -s kvsns_busybox ns_reset
+ln -s kvsns_busybox ns_mkdir
+ln -s kvsns_busybox ns_rmdir
+ln -s kvsns_busybox ns_cd
+ln -s kvsns_busybox ns_getattr
+
--- kvsns_shell/kvsns_busybox.c
+++ kvsns_shell/kvsns_busybox.c
@@ -1,37 +1,79 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <errno.h>
+#include <sys/param.h>
+#include <libgen.h>
 #include "../kvsns.h"
 
 int main(int argc, char *argv[])
 {
 	int rc;
-	kvsns_ino_t ino;
-	kvsns_ino_t ino2;
-	kvsns_ino_t parent;
-	struct stat bufstat;
-	struct stat bufstat2;
-	char key[KLEN];
-	char val[VLEN];
 	kvsns_cred_t cred;
+
+	char current_path[MAXPATHLEN];
+	char prev_path[MAXPATHLEN];
+	char exec_name[MAXPATHLEN];
+	char k[KLEN];
+	char v[VLEN];
+
+	kvsns_ino_t current_inode;
+	kvsns_ino_t parent_inode;
 
 	cred.uid = getuid();
 	cred.gid = getgid();
 
+	strncpy(exec_name, basename(argv[0]), MAXPATHLEN);
+
 	rc = kvsns_start();
 	if (rc != 0) {
-		fprintf(stderr, "kvsns_init: err=%d\n", rc);
+		fprintf(stderr, "kvsns_start: err=%d\n", rc);
 		exit(1);
 	}
 
-	rc = kvsns_init_root();
-	if (rc != 0) {
-		fprintf(stderr, "kvsns_init_root: err=%d\n", rc);
-		exit(1);
-	}
+	strcpy(k, "KVSNS_INODE");	
+	if (kvshl_get_char(k, v) == 0) 	
+		sscanf(v, "%llu", &current_inode);
 
+	strcpy(k, "KVSNS_PARENT_INODE");	
+	if (kvshl_get_char(k, v) == 0) 	
+		sscanf(v, "%llu", &parent_inode);
+
+	strcpy(k, "KVSNS_PATH");	
+	if (kvshl_get_char(k, v) == 0) 	
+		strcpy(current_path, v);
+
+	strcpy(k, "KVSNS_PREV_PATH");	
+	if (kvshl_get_char(k, v) == 0) 	
+		strcpy(prev_path, v);
+
+	printf("exec=%s -- ino=%llu, parent=%llu, path=%s prev=%s\n",
+		exec_name, current_inode, parent_inode,
+		current_path, prev_path);
+
+	/* Look at exec name and see what is to be done */
+	if (!strcmp(exec_name, "ns_reset")) {
+		strcpy(k, "KVSNS_INODE");
+		strcpy(v, "2");
+		kvshl_set_char(k, v);
+
+		strcpy(k, "KVSNS_PARENT_INODE");
+		strcpy(v, "2");
+		kvshl_set_char(k, v);
+
+		strcpy(k, "KVSNS_PATH");
+		strcpy(v, "/");
+		kvshl_set_char(k, v);
+
+		strcpy(k, "KVSNS_PREV_PATH");
+		strcpy(v, "/");
+		kvshl_set_char(k, v);
+	} else if (!strcmp(exec_name, "ns_mkdir")) {
+	} else if (!strcmp(exec_name, "ns_rmdir")) {
+	} else if (!strcmp(exec_name, "ns_cd")) {
+	} else if (!strcmp(exec_name, "ns_getattr")) {
+	} 
 	printf("######## OK ########\n");
 	return 0;
-
 }
