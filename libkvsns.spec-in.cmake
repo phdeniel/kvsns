fa4a997ef280bf3327f957b157aa01af46beeedc
Philippe DENIEL <philippe.deniel@cea.fr>
2016-08-01 17:11:18 +0200

    safer specfile
    To do:
    	- handle FS static and dynamic information (nb of dir, nb of files, inodes)
    	- handle struct stat as a binary
    	- handle a xattr as a binary value
    
    Signed-off-by: Philippe DENIEL <philippe.deniel@cea.fr>

--- libkvsns.spec-in.cmake
+++ libkvsns.spec-in.cmake
@@ -21,11 +21,23 @@ a Key-Value Store.
 Summary: Development file for the library libkvsns
 Group: Development/Libraries
 Requires: %{name} = %{version}-%{release} pkgconfig
+Requires: hiredis-devel
 
 %description devel
 The libkvsns is a library that allows of a POSIX namespace built on top of
 a Key-Value Store.
-This package contains the development headers for libkvsns.
+This package contains tools for libkvsns.
+
+%package utils
+Summary: Development file for the library libkvsns
+Group: Development/Libraries
+Requires: %{name} = %{version}-%{release} pkgconfig
+Requires: redis hiredis
+
+%description utils
+The libkvsns is a library that allows of a POSIX namespace built on top of
+a Key-Value Store.
+This package contains the tools for libkvsns.
 
 %prep
 %setup -q -n %{sourcename}
@@ -43,7 +55,7 @@ mkdir -p %{buildroot}%{_includedir}
 install -m 644 libkvsns.so %{buildroot}%{_libdir}
 install -m 644 kvsns.h  %{buildroot}%{_includedir}
 install -m 644 libkvsns.pc  %{buildroot}%{_libdir}/pkgconfig
-
+install -m 755 kvsns_shell/kvsns_busybox %{buildroot}%{_bindir}
 
 %clean
 rm -rf $RPM_BUILD_ROOT
@@ -57,6 +69,13 @@ rm -rf $RPM_BUILD_ROOT
 %{_libdir}/pkgconfig/libkvsns.pc
 %{_includedir}/kvsns.h
 
+%files utils
+%defattr(-,root,root)
+%{_bindir}/kvsns_busybox
+
 %changelog
-* Mon Jul 25 2016 Philippe DENIEL <philippe.deniel@cea.fr> 0.9.i
+* Mon Aug  1 2016 Philippe DENIEL <philippe.deniel@cea.fr> 0.9.2
+- Source code tree refurbished
+
+* Mon Jul 25 2016 Philippe DENIEL <philippe.deniel@cea.fr> 0.9.1
 - First alpha version
