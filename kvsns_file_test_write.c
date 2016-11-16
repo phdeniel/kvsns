26fbb14e505ef0597016d05ad034566c3608fe63
Philippe DENIEL <philippe.deniel@cea.fr>
2016-11-16 10:07:20 +0100

    Add LGPLv3 stuff in .c and .h files
    Change-Id: I93fdf13cbefc9cbc09f1b4d709c5c0e01597eac3
    Signed-off-by: Philippe DENIEL <philippe.deniel@cea.fr>

--- kvsns_file_test_write.c
+++ kvsns_file_test_write.c
@@ -1,3 +1,35 @@
+/*
+ * vim:noexpandtab:shiftwidth=8:tabstop=8:
+ *
+ * Copyright (C) CEA, 2016
+ * Author: Philippe Deniel  philippe.deniel@cea.fr
+ *
+ * contributeur : Philippe DENIEL   philippe.deniel@cea.fr
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * -------------
+ */
+
+/* kvsns_file_test_write.c
+ * KVSNS: simple write test
+ */
+
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
