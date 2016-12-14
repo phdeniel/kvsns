2a25f55f2128e880e1fdd0ce7b26e2f0f94e55da
Philippe DENIEL <philippe.deniel@cea.fr>
2016-12-14 17:13:41 +0100

    Add mero subdirectory
    Change-Id: Ib63e968f6cf2c7ce9f7037277f64772c24df3e72
    Signed-off-by: Philippe DENIEL <philippe.deniel@cea.fr>

--- kvsal/redis/kvsal_redis.c
+++ kvsal/redis/kvsal_redis.c
@@ -36,7 +36,7 @@
 #include <hiredis/hiredis.h>
 #include <pthread.h>
 #include <time.h>
-#include "kvsal.h"
+#include "../kvsal.h"
 
 /* The REDIS context exists in the TLS, for MT-Safety */
 __thread redisContext *rediscontext = NULL;
