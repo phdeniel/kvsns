db6944d13ec4596fc62ec523377835b2a1ad74d4
Philippe DENIEL <philippe.deniel@cea.fr>
2017-05-23 08:11:20 +0200

    Adding kvsns_copy.c to libkvsns
    Change-Id: Ic9f9c81df8185da734d972e9a0671591f777cddc
    Signed-off-by: Philippe DENIEL <philippe.deniel@cea.fr>

--- kvsns/kvsns_copy.c
+++ kvsns/kvsns_copy.c
@@ -41,3 +41,107 @@
 #include <kvsns/extstore.h>
 #include "kvsns_internal.h"
 
+#define BUFFSIZE 40960
+
+int kvsns_cp_from(kvsns_cred_t *cred, kvsns_file_open_t *kfd,
+		  int fd_dest, int iolen)
+{
+	off_t off;
+	ssize_t rsize, wsize;
+	int rc;
+	int remains;
+	size_t len;
+	char buff[BUFFSIZE];
+	bool eof;
+	struct stat stat;
+	size_t filesize;
+
+	rc = kvsns_getattr(cred, &kfd->ino, &stat);
+	if (rc < 0)
+		return rc;
+
+	filesize = stat.st_size;
+	remains = filesize;
+	off = 0LL;
+	while (off < filesize) {
+		len = (remains > iolen) ? iolen : remains;
+
+		rsize = kvsns_read(cred, kfd, buff, len, off);
+		if (rsize < 0)
+			return -1;
+
+		wsize = pwrite(fd_dest, buff, rsize, off);
+		if (wsize < 0)
+			return -1;
+
+		if (wsize != rsize)
+			return -1;
+
+		off += rsize;
+		remains -= rsize;
+	}
+
+	/* This POC writes on aligned blocks, we should align to real size */
+	/* Useful in this case ?? */
+	rc = ftruncate(fd_dest, filesize);
+	if (rc < 0)
+		return -1;
+
+	rc = fchmod(fd_dest, stat.st_mode);
+	if (rc < 0)
+		return -1;
+
+	return 0;
+}
+
+int kvsns_cp_to(kvsns_cred_t *cred, int fd_source,
+		kvsns_file_open_t *kfd, int iolen)
+{
+	off_t off;
+	ssize_t rsize, wsize;
+	int remains;
+	size_t len;
+	char buff[BUFFSIZE];
+	bool stable;
+	int rc;
+	struct stat srcstat;
+	size_t filesize;
+
+	rc = fstat(fd_source, &srcstat);
+	if (rc < 0)
+		return -errno;
+
+	remains = srcstat.st_size;
+	filesize = srcstat.st_size;
+	off = 0LL;
+	while (off < filesize) {
+		len = (remains > iolen) ? iolen : remains;
+
+		rsize = pread(fd_source, buff, len, off);
+		if (rsize < 0)
+			return -1;
+
+		wsize = kvsns_write(cred, kfd, buff, rsize, off);
+		if (wsize < 0)
+			return -1;
+
+		if (wsize != rsize)
+			return -1;
+
+		if (wsize < 0)
+			return -1;
+
+		if (wsize != rsize)
+			return -1;
+
+		off += rsize;
+		remains -= rsize;
+	}
+
+	rc = kvsns_setattr(cred, &kfd->ino, &srcstat, STAT_MODE_SET);
+	if (rc < 0)
+		return rc;
+
+	return 0;
+}
+
