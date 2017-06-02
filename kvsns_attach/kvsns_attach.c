13ce755e741f8aa7fe47e87b94ee86f7b13ceeb1
Philippe DENIEL <philippe.deniel@cea.fr>
2017-06-02 14:33:05 +0200

    kvsns_attach: WiP: build stat structure
    Signed-off-by: Philippe DENIEL <philippe.deniel@cea.fr>

--- kvsns_attach/kvsns_attach.c
+++ kvsns_attach/kvsns_attach.c
@@ -47,21 +47,68 @@ static int verbose_flag;
 static void help(char *exec)
 {
 	printf("%s  \t[-u <user>] [--user=<user>]	set user\n"
-	       "\t	[-g <group>] [--group=<group>]	set group\n"
-	      "\t	[-m <mode>] [--mode=<mode>]	set mode (octal)\n"
-	      "\t	[-s <size>] [--size=<size>]	set size\n"
-	      "\t	[-a <atime>] [--atime=<atime>]	set atime\n"
-	      "\t	[-m <mtime>] [--mtime=<mtime>]	set mtime\n"
-	      "\t	[-c <ctime>] [--ctime=<ctime>]	set ctime\n"
-	      "\t	-p <path> --path=<path>		path in kvsns\n"
-	      "\t	-o <objid> --objid=<objid>	objectid\n", exec);
+		"\t	[-g <group>] [--group=<group>]	set group\n"
+		"\t	[-M <mode>] [--mode=<mode>]	set mode (octal)\n"
+		"\t	[-s <size>] [--size=<size>]	set size\n"
+		"\t	[-a <atime>] [--atime=<atime>]	set atime\n"
+		"\t	[-m <mtime>] [--mtime=<mtime>]	set mtime\n"
+		"\t	[-c <ctime>] [--ctime=<ctime>]	set ctime\n"
+		"\t	-p <path> --path=<path>		path in kvsns\n"
+		"\t	-o <objid> --objid=<objid>	objectid\n"
+		"\t 	date format is 'now' or 10/06/2017 16:06:23", exec);
 
 	exit(0);
+}
+
+static int str2time(char *arg, time_t *res)
+{
+	struct tm timeval;
+	int tnum;
+	int rc;
+
+	tnum = atoi(arg);
+	if (tnum != 0) {
+		*res = tnum;
+		return 0;
+	}
+
+	if (!strcmp(arg, "now")) {
+		if (time(res) < 0) {
+			fprintf(stderr, "Can't get current time\n");
+			exit(1);
+		}
+		return 0;
+	}
+
+	/* Arg is a tring, format is 10/06/2017 16:06:23 */
+	if (!strptime(arg, "%d/%m/%y %H:%M:%S", &timeval))
+		return -1;
+
+	*res = mktime(&timeval);
+	return 0;
+}
+
+static void default_stat(struct stat *stat)
+{
+	if (!stat)
+		return;
+
+	memset(stat, 0, sizeof(struct stat));
+	stat->st_uid = getuid();
+	stat->st_gid = getgid();
+	stat->st_mode = S_IFREG|0644;
 }
 
 int main(int argc, char **argv)
 {
 	int c;
+	struct stat stat;
+	int statflags;
+	char path[MAXPATHLEN];
+	char objid[MAXPATHLEN];
+
+	/* stat structure with default values */
+	default_stat(&stat);
 
 	while (1) {
 		static struct option long_options[] = {
@@ -85,7 +132,7 @@ int main(int argc, char **argv)
 		/* getopt_long stores the option index here. */
 		int option_index = 0;
 
-		c = getopt_long (argc, argv, "vhu:g:p:o:s:m:a:c",
+		c = getopt_long (argc, argv, "vhu:g:p:o:M:s:m:a:c",
 		long_options, &option_index);
 
 		/* Detect the end of the options. */
@@ -102,6 +149,58 @@ int main(int argc, char **argv)
 			if (optarg)
 				printf(" with arg %s", optarg);
 			printf("\n");
+
+			/* Do the config job */
+			if (!strcmp(long_options[option_index].name,
+				   "help"))
+				help(basename(argv[0]));
+			else if (!strcmp(long_options[option_index].name,
+				   "user")) {
+				statflags |= STAT_UID_SET;
+				stat.st_uid = atoi(optarg);
+			} else if (!strcmp(long_options[option_index].name,
+				  "group")) {
+				statflags |= STAT_GID_SET;
+				stat.st_gid = atoi(optarg);
+			} else if (!strcmp(long_options[option_index].name,
+				  "path"))
+				strncpy(path, optarg, MAXPATHLEN);
+			else if (!strcmp(long_options[option_index].name,
+				  "objid"))
+				strncpy(objid, optarg, MAXPATHLEN);
+			else if (!strcmp(long_options[option_index].name,
+				  "mode")) {
+				statflags |= STAT_MODE_SET;
+				stat.st_mode = S_IFREG|strtoul(optarg, 0, 8);
+			} else if (!strcmp(long_options[option_index].name,
+				  "size")) {
+				statflags |= STAT_SIZE_SET;
+				stat.st_size = atoi(optarg);
+			} else if (!strcmp(long_options[option_index].name,
+				  "atime")) {
+				statflags |= STAT_ATIME_SET;
+				if (str2time(optarg, &stat.st_atime) < 0) {
+					fprintf(stderr,
+						"Error: bad atime arg\n");
+					exit(1);
+				}
+			} else if (!strcmp(long_options[option_index].name,
+				  "mtime")) {
+				statflags |= STAT_MTIME_SET;
+				if (str2time(optarg, &stat.st_mtime) < 0) {
+					fprintf(stderr,
+						"Error: bad mtime arg\n");
+					exit(1);
+				}
+			} else if (!strcmp(long_options[option_index].name,
+				  "ctime")) {
+				statflags |= STAT_CTIME_SET;
+				if (str2time(optarg, &stat.st_ctime) < 0) {
+					fprintf(stderr,
+						"Error: bad ctime arg\n");
+					exit(1);
+				}
+			}
 			break;
 
 		case 'v':
@@ -110,34 +209,56 @@ int main(int argc, char **argv)
 
 		case 'u':
 			printf("option -u with value `%s'\n", optarg);
+			statflags |= STAT_UID_SET;
+			stat.st_uid = atoi(optarg);
 			break;
 
 		case 'g':
 			printf("option -g with value `%s'\n", optarg);
+			statflags |= STAT_GID_SET;
+			stat.st_gid = atoi(optarg);
 			break;
 
 		case 'p':
-			printf("option -p with value `%s'\n", optarg);
+			strncpy(path, optarg, MAXPATHLEN);
 			break;
 
 		case 'o':
-			printf("option -o with value `%s'\n", optarg);
+			strncpy(objid, optarg, MAXPATHLEN);
 			break;
 
 		case 's':
-			printf("option -s with value `%s'\n", optarg);
+			statflags |= STAT_SIZE_SET;
+			stat.st_size = atoi(optarg);
 			break;
 
-		case 'm':
-			printf("option -m with value `%s'\n", optarg);
+		case 'M':
+			statflags |= STAT_MODE_SET;
+			stat.st_mode = S_IFREG|strtoul(optarg, 0, 8);
 			break;
 
 		case 'a':
-			printf("option -a with value `%s'\n", optarg);
+			statflags |= STAT_ATIME_SET;
+			if (str2time(optarg, &stat.st_atime) < 0) {
+				fprintf(stderr, "Error: bad mtime arg\n");
+				exit(1);
+			}
+			break;
+
+		case 'm':
+			statflags |= STAT_MTIME_SET;
+			if (str2time(optarg, &stat.st_mtime) < 0) {
+				fprintf(stderr, "Error: bad mtime arg\n");
+				exit(1);
+			}
 			break;
 
 		case 'c':
-			printf("option -c with value `%s'\n", optarg);
+			statflags |= STAT_CTIME_SET;
+			if (str2time(optarg, &stat.st_ctime) < 0) {
+				fprintf(stderr, "Error: bad ctime arg\n");
+				exit(1);
+			}
 			break;
 
 		case 'h':
