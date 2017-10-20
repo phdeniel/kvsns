2256267e82f6c54387cee90b411f324e1ca3dce8
Philippe DENIEL <philippe.deniel@cea.fr>
2017-10-20 11:08:21 +0200

    WiP: 1st version of API functions
    Change-Id: Ibcd865e2cbd62ef02510ca7d91f4d6dfeb3aa759
    Signed-off-by: Philippe DENIEL <philippe.deniel@cea.fr>

--- extstore/rados/extstore.c
+++ extstore/rados/extstore.c
@@ -43,15 +43,43 @@
 	if (__rc != 0)        \
 		goto __label; })
 
+#define CEPH_CONFIG_DEFAULT "/etc/ceph/ceph.conf"
+
 static char pool[MAXNAMLEN];
+__thread rados_t cluster;
+
+static void build_objid(kvsns_ino_t ino, char *objid, int objidlen)
+{
+	if (!ino)
+		return;
+
+	snprintf(objid, objidlen, "kvsns.%llu", ino);
+}
 
 int extstore_create(kvsns_ino_t object)
 {
+	int rc;
+	rados_ioctx_t io;
+	char objid[MAXNAMLEN];
+
+	build_objid(object, objid, MAXNAMLEN);
+
+	rc = rados_ioctx_create(cluster, pool, &io);
+	if (rc < 0)
+		return rc;
+
+	rc = rados_write(io, objid, "", 0, 0);
+	if (rc < 0)
+		return rc;
+
+	rados_ioctx_destroy(io);
 	return 0;
 }
 
 int extstore_attach(kvsns_ino_t *ino, char *objid, int objid_len)
 {
+	/* In the case of RADOS, nothing is to be done for the
+	 * object store manages its md */
 	return 0;
 }
 
@@ -59,6 +87,12 @@ int extstore_init(struct collection_item *cfg_items)
 {
 	struct collection_item *item;
 	int rc;
+	char clustername[MAXNAMLEN];
+	char user[MAXNAMLEN];
+	char ceph_conf[MAXPATHLEN];
+
+	if (cfg_items == NULL)
+		return -EINVAL;
 
 	/* Get config from ini file */
 	item = NULL;
@@ -72,11 +106,74 @@ int extstore_init(struct collection_item *cfg_items)
 	strncpy(pool, get_string_config_value(item, NULL),
 		MAXNAMLEN);
 
+	item = NULL;
+	rc = get_config_item("rados", "cluster",
+			      cfg_items, &item);
+	if (rc != 0)
+		return -rc;
+	if (item == NULL)
+		return -EINVAL;
+	strncpy(clustername, get_string_config_value(item, NULL),
+		MAXNAMLEN);
+
+	item = NULL;
+	rc = get_config_item("rados", "user",
+			      cfg_items, &item);
+	if (rc != 0)
+		return -rc;
+	if (item == NULL)
+		return -EINVAL;
+	strncpy(user, get_string_config_value(item, NULL),
+		MAXNAMLEN);
+
+	item = NULL;
+	rc = get_config_item("rados", "config",
+			      cfg_items, &item);
+	if (rc != 0)
+		return -rc;
+	if (item == NULL)
+		strncpy(ceph_conf, CEPH_CONFIG_DEFAULT, MAXPATHLEN);
+	else
+		strncpy(ceph_conf, get_string_config_value(item, NULL),
+			MAXPATHLEN);
+
+	/* Rados init */
+	rc = rados_create2(&cluster, clustername, user, 0LL);
+	if (rc < 0)
+		return rc;
+
+	rc = rados_conf_read_file(cluster, ceph_conf);
+	if (rc < 0)
+		return rc;
+
+	/* Connect to the cluster */
+	rc = rados_connect(cluster);
+	if (rc < 0)
+		return rc;
+
 	return 0;
 }
 
 int extstore_del(kvsns_ino_t *ino)
 {
+	int rc;
+	rados_ioctx_t io;
+	char objid[MAXNAMLEN];
+
+	if (!ino)
+		return -EINVAL;
+
+	build_objid(*ino, objid, MAXNAMLEN);
+
+	rc = rados_ioctx_create(cluster, pool, &io);
+	if (rc < 0)
+		return rc;
+
+	rc = rados_remove(io, objid);
+	if (rc < 0)
+		return rc;
+
+	rados_ioctx_destroy(io);
 	return 0;
 }
 
@@ -87,7 +184,37 @@ int extstore_read(kvsns_ino_t *ino,
 		  bool *end_of_file,
 		  struct stat *stat)
 {
-	return 0;
+	int rc;
+	rados_ioctx_t io;
+	char objid[MAXNAMLEN];
+	uint64_t size;
+	time_t mtime;
+	int read;
+
+	if (!ino)
+		return -EINVAL;
+
+	build_objid(*ino, objid, MAXNAMLEN);
+
+	rc = rados_ioctx_create(cluster, pool, &io);
+	if (rc < 0)
+		return rc;
+
+	read = rados_read(io, objid, buffer, buffer_size, offset);
+	if (read < 0)
+		return read;
+
+	rc = rados_stat(io, objid, &size, &mtime);
+	if (rc < 0)
+		return rc;
+
+	rados_ioctx_destroy(io);
+
+	stat->st_size = size;
+	stat->st_mtime = mtime;
+	stat->st_atime = mtime; /* @todo bug ?*/
+
+	return read;
 }
 
 int extstore_write(kvsns_ino_t *ino,
@@ -97,7 +224,37 @@ int extstore_write(kvsns_ino_t *ino,
 		   bool *fsal_stable,
 		   struct stat *stat)
 {
-	return 0;
+	int rc;
+	rados_ioctx_t io;
+	char objid[MAXNAMLEN];
+	uint64_t size;
+	time_t mtime;
+	int written;
+
+	if (!ino)
+		return -EINVAL;
+
+	build_objid(*ino, objid, MAXNAMLEN);
+
+	rc = rados_ioctx_create(cluster, pool, &io);
+	if (rc < 0)
+		return rc;
+
+	written = rados_write(io, objid, buffer, buffer_size, offset);
+	if (written < 0)
+		return written;
+
+	rc = rados_stat(io, objid, &size, &mtime);
+	if (rc < 0)
+		return rc;
+
+	rados_ioctx_destroy(io);
+
+	stat->st_size = size;
+	stat->st_mtime = mtime;
+	stat->st_atime = mtime; /* @todo bug ?*/
+
+	return written;
 }
 
 
@@ -106,11 +263,65 @@ int extstore_truncate(kvsns_ino_t *ino,
 		      bool on_obj_store,
 		      struct stat *stat)
 {
+	int rc;
+	rados_ioctx_t io;
+	char objid[MAXNAMLEN];
+	uint64_t size;
+	time_t mtime;
+
+	if (!ino || !stat)
+		return -EINVAL;
+
+	build_objid(*ino, objid, MAXNAMLEN);
+
+	rc = rados_ioctx_create(cluster, pool, &io);
+	if (rc < 0)
+		return rc;
+
+	rc = rados_trunc(io, objid, (uint64_t)filesize);
+	if (rc < 0)
+		return rc;
+
+	rc = rados_stat(io, objid, &size, &mtime);
+	if (rc < 0)
+		return rc;
+
+	rados_ioctx_destroy(io);
+
+	stat->st_size = size;
+	stat->st_mtime = mtime;
+	stat->st_atime = mtime; /* @todo bug ?*/
+
 	return 0;
 }
 
 int extstore_getattr(kvsns_ino_t *ino,
 		     struct stat *stat)
 {
+	int rc;
+	rados_ioctx_t io;
+	char objid[MAXNAMLEN];
+	uint64_t size;
+	time_t mtime;
+
+	if (!ino || !stat)
+		return -EINVAL;
+
+	build_objid(*ino, objid, MAXNAMLEN);
+
+	rc = rados_ioctx_create(cluster, pool, &io);
+	if (rc < 0)
+		return rc;
+
+	rc = rados_stat(io, objid, &size, &mtime);
+	if (rc < 0)
+		return rc;
+
+	rados_ioctx_destroy(io);
+
+	stat->st_size = size;
+	stat->st_mtime = mtime;
+	stat->st_atime = mtime; /* @todo bug ?*/
+
 	return 0;
 }
