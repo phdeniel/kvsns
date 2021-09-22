5193c0eaea43e9c6ec6bb315b22d8fc04e6d7ff4
Philippe DENIEL <philippe.deniel@cea.fr>
2021-09-22 04:25:58 -0400

    Add bulk copy_from in extstore/motr
    Signed-off-by: Philippe DENIEL <philippe.deniel@cea.fr>

--- extstore/motr/m0store_cp_from_bulk.c
+++ extstore/motr/m0store_cp_from_bulk.c
@@ -17,46 +17,50 @@ static struct collection_item *cfg_items;
 int copy_from_mero(struct m0_uint128 id, int fd_dest,
 		   int iolen, size_t filesize)
 {
-	off_t off;
 	size_t rsize, wsize;
-	int rc;
-	int remains;
-	size_t len;
 	ssize_t bsize;
 	char *buff;
+
+	size_t remain;
+	size_t aligned_offset;
+	size_t nb;
+
+	int rc;
 
 	bsize = m0store_get_bsize(id);
 	if (bsize < 0)
 		return bsize;
 
-	buff = malloc(bsize*M0STORE_BLK_COUNT);
+	remain = filesize % bsize;
+	nb = filesize / bsize;
+	aligned_offset = nb * bsize;
+
+	printf("filesize=%zd bsize=%zd nb=%zd remain=%zd aligned_offset=%zd\n",
+		filesize, bsize, nb, remain, aligned_offset);
+
+	buff = malloc(bsize);
 	if (buff == NULL)
 		return -ENOMEM;
 
-	remains = filesize;
-	off = 0LL;
-	while (off < filesize) {
-		len = (remains > iolen) ? iolen : remains;
+	rc = m0_read_bulk(fd_dest,
+			  id,
+			  bsize,
+			  nb,
+			  0,
+			  0,
+			  0);
+	printf("===> rc=%d\n", rc);
 
-		rsize = m0store_pread(id, off, len, bsize, buff);
-		if (rsize < 0) {
-			free(buff);
-			return -1;
-		}
+	rsize = m0store_pread(id, aligned_offset, remain, bsize, buff);
+	if (rsize < 0) {
+		free(buff);
+		return -1;
+	}
 
-		wsize = pwrite(fd_dest, buff, rsize, off);
-		if (wsize < 0) {
-			free(buff);
-			return -1;
-		}
-
-		if (wsize != rsize) {
-			free(buff);
-			return -1;
-		}
-
-		off += rsize;
-		remains -= rsize;
+	wsize = pwrite(fd_dest, buff, rsize, aligned_offset);
+	if (wsize < 0) {
+		free(buff);
+		return -1;
 	}
 
 	/* This POC writes on aligned blocks, we should align to real size */
@@ -103,16 +107,16 @@ int main(int argc, char *argv[])
 	iolen = atoi(argv[3]);
 	printf("Start: iolen=%u\n", iolen);
 
-        /* hande object id */
-        if (m0_obj_id_sscanf(argv[1], &id) < 0) {
-                fprintf(stderr, "Bad Id\n");
-                exit(1);
-        }
+	/* hande object id */
+	if (m0_obj_id_sscanf(argv[1], &id) < 0) {
+		fprintf(stderr, "Bad Id\n");
+		exit(1);
+	}
 
-        if (!entity_id_is_valid(&id)) {
-                fprintf(stderr, "Entity Id is invalid\n");
-                exit(1);
-        }
+	if (!entity_id_is_valid(&id)) {
+		fprintf(stderr, "Entity Id is invalid\n");
+		exit(1);
+	}
 
 
 	filesize = atoi(argv[4]);
